386c6e65c53f1ec2dd0dfccb69c842cf5e90ffa7
Zac Ong <61817066+zzacong@users.noreply.github.com>
2022-07-03 18:16:43 +1000

    fix build type error

--- src/pages/api/examples.ts
+++
@@ -1,7 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from 'next'
-import { prisma } from '$server/db/client'
-
-export default async function handelr(req: NextApiRequest, res: NextApiResponse) {
-  const examples = await prisma.example.findMany()
-  res.status(200).json(examples)
-}
