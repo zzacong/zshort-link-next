956ab394ba5dd6327b0f94575dd0c2fbb61a13c4
zzacong <61817066+zzacong@users.noreply.github.com>
2023-05-13 16:14:59 +1000

    form enhancement

--- src/components/CreateLink.tsx
+++ src/components/CreateLink.tsx
@@ -1,70 +1,63 @@
 import type { ChangeEventHandler, FormEventHandler } from 'react';
-import { useCallback, useState } from 'react';
 
+import { useState } from 'react';
 import clsx from 'clsx';
 import random from 'random-words';
 import debounce from 'lodash/debounce';
 import { ClipboardIcon, ArrowPathIcon } from '@heroicons/react/24/outline';
 import { Popover } from '@headlessui/react';
 import { AnimatePresence, motion } from 'framer-motion';
+import { toast } from 'react-hot-toast';
 import copy from 'copy-to-clipboard';
 
 import { api } from '~/lib/api';
 
-type Form = {
-  slug: string;
-  url: string;
-};
-
 export default function CreateLinkForm() {
-  const [form, setForm] = useState<Form>({ slug: '', url: '' });
+  const [slug, setSlug] = useState('');
   const url = `${window.location.origin}/r`;
 
   const slugCheck = api.slug.slugCheck.useQuery(
-    { slug: form.slug },
+    { slug },
     {
+      enabled: !!slug,
       refetchOnReconnect: false,
       refetchOnMount: false,
       refetchOnWindowFocus: false,
     }
   );
-  const createSlug = api.slug.createSlug.useMutation();
-
-  const onChange: ChangeEventHandler<HTMLInputElement> = useCallback(
-    e => {
-      setForm({
-        ...form,
-        slug: e.target.value,
-      });
-      debounce(slugCheck.refetch, 100);
+  const createSlug = api.slug.createSlug.useMutation({
+    onError(error) {
+      const message = error.data?.zodError?.fieldErrors.url;
+      if (message?.[0]) toast.error(message[0]);
+      else toast.error('Failed to create. Please try again later.');
     },
-    [form, slugCheck.refetch]
-  );
+  });
 
-  const onRandom = useCallback(() => {
+  const onChange: ChangeEventHandler<HTMLInputElement> = e => {
+    setSlug(e.target.value);
+    debounce(slugCheck.refetch, 100);
+  };
+
+  const onRandom = () => {
     const slug = random({ min: 1, max: 2, join: '-' });
-    setForm({
-      ...form,
-      slug,
-    });
+    setSlug(slug);
     void slugCheck.refetch();
-  }, [form, slugCheck]);
+  };
 
-  const onSubmit: FormEventHandler<HTMLFormElement> = useCallback(
-    e => {
-      e.preventDefault();
-      createSlug.mutate({ ...form });
-    },
-    [createSlug, form]
-  );
+  const onSubmit: FormEventHandler<HTMLFormElement> = e => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    let url = formData.get('url') as string;
+    if (!url) return toast.error('URL cannot be empty');
+    if (!/^https?:\/\//.test(url)) url = `https://${url}`;
+    if (!/.*(\.).*/.test(url)) return toast.error('Invalid URL');
+    createSlug.mutate({ slug, url });
+  };
 
-  const onCopy = useCallback(
-    (close: () => void) => {
-      setTimeout(() => close(), 1200);
-      copy(`${url}/${form.slug}`);
-    },
-    [form.slug, url]
-  );
+  const onCopy = (close: () => void) => {
+    setTimeout(() => close(), 1200);
+    copy(`${url}/${slug}`);
+  };
 
   return (
     <div className="w-full max-w-xl md:w-2/3 xl:w-1/2">
@@ -75,7 +68,7 @@ export default function CreateLinkForm() {
       {createSlug.status === 'success' ? (
         <div className="flex flex-col gap-4">
           <div className="flex gap-2 md:gap-4">
-            <h2 className="flex-grow break-all rounded bg-white/90 px-4 py-2 font-mono text-lg text-gray-800 lg:text-xl">{`${url}/${form.slug}`}</h2>
+            <h2 className="flex-grow break-all rounded bg-white/90 px-4 py-2 font-mono text-lg text-gray-800 lg:text-xl">{`${url}/${slug}`}</h2>
 
             <Popover className="relative flex">
               {({ open, close }) => (
@@ -115,7 +108,7 @@ export default function CreateLinkForm() {
             className="cursor-pointer rounded bg-blue-500 px-4 py-2 font-bold hover:bg-blue-600"
             onClick={() => {
               createSlug.reset();
-              setForm({ slug: '', url: '' });
+              setSlug('');
             }}
           >
             Reset
@@ -131,6 +124,7 @@ export default function CreateLinkForm() {
             <div className="flex flex-1 flex-wrap gap-2 md:gap-4">
               <input
                 type="text"
+                name="slug"
                 onChange={onChange}
                 minLength={1}
                 placeholder="rothaniel"
@@ -138,7 +132,7 @@ export default function CreateLinkForm() {
                   'input',
                   slugCheck.isFetched && slugCheck.data?.used && 'border-red-500 text-red-500'
                 )}
-                value={form.slug}
+                value={slug}
                 pattern={'^[-a-zA-Z0-9]+$'}
                 title="Only alphanumeric characters and hypens are allowed. No spaces."
                 required
@@ -150,11 +144,14 @@ export default function CreateLinkForm() {
           </div>
 
           <div className="flex flex-col gap-2 sm:flex-row sm:items-center md:gap-4">
-            <span className="text-lg font-medium">Link</span>
+            <label htmlFor="url" className="text-lg font-medium">
+              Link
+            </label>
             <input
-              type="url"
-              onChange={e => setForm({ ...form, url: e.target.value })}
-              placeholder="https://google.com"
+              type="text"
+              id="url"
+              name="url"
+              placeholder="https://zzacong.com"
               className="input"
               required
             />
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,16 @@
 import type { AppType } from 'next/app';
+import { Toaster } from 'react-hot-toast';
 
 import { api } from '~/lib/api';
 import '~/styles/globals.css';
 
 const MyApp: AppType = ({ Component, pageProps: { ...pageProps } }) => {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Toaster position="bottom-center" />
+      <Component {...pageProps} />;
+    </>
+  );
 };
 
 export default api.withTRPC(MyApp);
